c874baece7ec638a8907ed946e04dd559e8d8d76
Dipak Telangre <dipaktelangre@gmail.com>
2026-01-11 19:36:39 +0530

    Add more index

--- api/market.js
+++ api/market.js
@@ -51,5 +51,70 @@ router.get('/nifty', cors(), bodyParser.json(), (req, res) => {
         });
 });
 
+// get popular stocks
+router.get('/popular-stocks', cors(), bodyParser.json(), (req, res) => {
+    // fetch data from a financial API https://api.stockedge.com/Api/PopularStocksDashboardApi/GetPopularStocks?lang=en
+    const apiUrl = "https://api.stockedge.com/Api/PopularStocksDashboardApi/GetPopularStocks?lang=en";
+
+    axios.get(apiUrl)
+        .then(response => {
+            console.log("API response", response.data);
+            const popularStocks = response.data.map((item) => ({                        
+                companyName: item.Name,
+                currentPrice: item.LTP,                        
+                changePercent: item.CZG
+            }));
+            res.json(popularStocks);
+        })
+        .catch(error => {
+            console.error('Error fetching popular stocks data:', error);
+            res.status(500).json({ error: 'Failed to fetch popular stocks data' });
+        });
+});
+
+
+// get bank nifty
+router.get('/bank-nifty', cors(), bodyParser.json(), (req, res) => {
+    // fetch data from a financial API https://api.stockedge.com/api/LatestListingPriceDashboardApi/GetLatestListingPrice/17483?lang=en
+    const apiUrl = "https://api.stockedge.com/api/LatestListingPriceDashboardApi/GetLatestListingPrice/17483?lang=en";
+    axios.get(apiUrl)
+        .then(response => {
+            console.log("API response", response.data);
+            const bankNiftyData = {
+                index: 'Bank Nifty',
+                currentPrice: response.data.C,
+                change: response.data.CZ,
+                changePercent: response.data.CZG
+            };
+            res.json(bankNiftyData);
+        })
+        .catch(error => {
+            console.error('Error fetching Bank Nifty data:', error);
+            res.status(500).json({ error: 'Failed to fetch Bank Nifty data' });
+        });
+});
+
+// get nifty 500 
+
+router.get('/nifty-500', cors(), bodyParser.json(), (req, res) => {
+    // fetch data from a financial API https://api.stockedge.com/api/LatestListingPriceDashboardApi/GetLatestListingPrice/17496?lang=en
+    const apiUrl = "https://api.stockedge.com/api/LatestListingPriceDashboardApi/GetLatestListingPrice/17496?lang=en";
+    axios.get(apiUrl)
+        .then(response => {
+            console.log("API response", response.data);
+            const nifty500Data = {
+                index: 'Nifty 500',
+                currentPrice: response.data.C,
+                change: response.data.CZ,
+                changePercent: response.data.CZG
+            };
+            res.json(nifty500Data);
+        })
+        .catch(error => {
+            console.error('Error fetching Nifty 500 data:', error);
+            res.status(500).json({ error: 'Failed to fetch Nifty 500 data' });
+        });
+});
+
 
 module.exports = router;
